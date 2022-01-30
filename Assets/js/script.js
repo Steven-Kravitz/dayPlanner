7b6d3f93e57628a7528dd5e88137744be8ba3d2c
Steven Kravitz <Steven.Kravitz95@gmail.com>
2022-01-30 15:14:42 -0500

    built logic for hour display row in planner

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,15 +4,15 @@
 // Header has display of month and day of the week. Could add active clock to this too
 
 // Create variables for present Month, day of month, and day of week.
-var presentDOW = moment().format('dddd')
+var presentDOW = moment().format('dddd');
 // console.log(presentDOW)
-var presentMonth = moment().format('MMMM')
+var presentMonth = moment().format('MMMM');
 // console.log(presentMonth)
-var presentDOM = moment().format('do')
+var presentDOM = moment().format('Do');
 // console.log(presentDOM)
 
 // List the current date as text in jumbrotron, html line 29 is your target
-$('#currentDay').text(presentDOW + ', ' + presentMonth + ' ' + presentDOM)
+$('#currentDay').text(presentDOW + ', ' + presentMonth + ' ' + presentDOM);
 
 // Create timeblock x 10 (8am - 6pm), you need a space on left for time of day, middle note area, a submit button on right to save text
 
@@ -21,7 +21,18 @@ for (var hour = 8; hour <= 18; hour++) {
     var hIndex = hour - 8;
 
     // Build the row base
-    $('<div>').addClass('row')
-    $('<div>').addClass('planRow')
-    
+    $('<div>').addClass('row');
+    $('<div>').addClass('planRow');
+    $('<div>').attr('hIndex', hour);
+
+    // Build time of day part of row
+    var hourTOD = 0;
+    var amPM = "";
+    if (hour > 12) {
+        hourTOD = hour - 12;
+        amPM = "pm";
+    } else {
+        hourTOD = hour;
+        amPM = "am";
+    }
 }
